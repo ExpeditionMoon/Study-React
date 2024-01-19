cb67f88a617cbc1bdac1529b194e44ce8410d510
ExpeditionMoon <dnjswon1025@gmail.com>
2024-01-19 23:45:42 +0900

    cart 기능 구현 & Custom Hooks 생성
    - cart 기능 구현
    - Custom Hooks 생성

--- shopping-mall/src/App.js
+++ shopping-mall/src/App.js
@@ -3,16 +3,15 @@ import "firebase/auth";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import ProductDetail from "./components/productList/ProductDetail";
-import ProductLists from "./components/productList/ProductLists";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-          <Route path="/" element={<Home />} >
-            <Route path="/products" element={<ProductLists />}  />
-          </ Route>
-          <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/" element={<Home />}/>
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
--- shopping-mall/src/components/Cart/Cart.js
+++ shopping-mall/src/components/Cart/Cart.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./Cart.css";
+import Header from "../header/Header";
 
 export default function Cart() {
+  const [cart, setCart] = useState([]);
+
+  // 장바구니 불러오기
+  useEffect(() => {
+    const savedCart = localStorage.getItem("cart");
+    if (savedCart) {
+      setCart(JSON.parse(savedCart));
+    }
+  }, []);
+
+  const removeCart = (product) => {
+    const newCart = cart.filter((item) => item.id !== product.id);
+    setCart(newCart);
+    localStorage.setItem("cart", JSON.stringify(newCart));
+  };
+
   return (
-    <div>Cart</div>
-  )
+    <div className="home">
+      <Header />
+      <div className="cartBox">
+        <h2>장바구니</h2>
+        {cart.map((product) => (
+          <div className="cartDetail" key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <div className="cartInfo">
+              <h3>{product.title}</h3>
+              <h2>${product.price}</h2>
+            </div>
+            <button onClick={() => removeCart(product)}>삭제</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
